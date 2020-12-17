7219cf790beb1c3ef07b64c71508bfaadf2a0d69
JohnHa0 <8759237+JohnHa0@users.noreply.github.com>
2020-12-17 17:16:03 +0800

    更正Cookie大小写

--- smCookie.js
+++ smCookie.js
@@ -2,25 +2,25 @@
 此文件为Node.js专用。其他用户请忽略
  */
 //此处填写京东账号cookie。
-//注：github action用户cookie填写到Settings-Secrets里面，新增SM_Cookie，多个账号的cookie使用`&`隔开或者换行
+//注：github action用户cookie填写到Settings-Secrets里面，新增SM_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let TokenSMs = [
   '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
-if (process.env.SM_Cookie) {
-  if (process.env.SM_Cookie.indexOf('&') > -1) {
+if (process.env.SM_COOKIE) {
+  if (process.env.SM_COOKIE.indexOf('&') > -1) {
     console.log(`您的cookie选择的是用&隔开\n`)
-    TokenSMs = process.env.SM_Cookie.split('&');
-  } else if (process.env.SM_Cookie.indexOf('\n') > -1) {
+    TokenSMs = process.env.SM_COOKIE.split('&');
+  } else if (process.env.SM_COOKIE.indexOf('\n') > -1) {
     console.log(`您的cookie选择的是用换行隔开\n`)
-    TokenSMs = process.env.SM_Cookie.split('\n');
-  } else if (process.env.SM_Cookie.indexOf('\\n') > -1) {
+    TokenSMs = process.env.SM_COOKIE.split('\n');
+  } else if (process.env.SM_COOKIE.indexOf('\\n') > -1) {
     //环境变量兼容腾讯云和docker下\n会被转义成\\n
     console.log(`您的cookie选择的是用换行隔开\\n`)
-    TokenSMs = process.env.SM_Cookie.split('\\n');
+    TokenSMs = process.env.SM_COOKIE.split('\\n');
   } else {
-    TokenSMs = [process.env.SM_Cookie];
+    TokenSMs = [process.env.SM_COOKIE];
   }
   TokenSMs = [...new Set(TokenSMs)]
   console.log(`\n====================共有${TokenSMs.length}个京东账号Cookie=========\n`);
